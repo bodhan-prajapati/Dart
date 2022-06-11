1edbff0a8518ce7866b7e95bb4a345b1240c76c6
bodhan-prajapati <107033509+bodhan-prajapati@users.noreply.github.com>
2022-06-11 15:23:54 +0530

    Initial Commit

---
+++ input.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+void main()
+{
+  print("enter  number");
+
+  int? n= int.parse(stdin.readLineSync()!);
+  print("your fav number ");
+}
---
+++ operator.dart
@@ -0,0 +1,63 @@
+void main() {
+  // print("hello");
+
+  //incerment operator
+
+  // var i=3;
+  // print(i);
+  // print(++i);
+  // print(i);
+
+  //decrement operator
+
+  // var i=3;
+  // print(i);
+  // print(--i);
+  // print(i);
+
+  //pelatniol operators
+
+  // var a=100;
+  // var b=200;
+  // print(a==b);
+  // print(a!=b);
+  // print(a>b);
+  // print(a<b);
+  // print(a>=b);
+  // print(a<=b);
+
+  // Data type
+
+  //int a = 10; // a variable ka mame hai and 10 variable ka value hai
+  //print(a);
+
+  //bool Data type
+
+  // bool a=true;
+  // print(a);
+
+  // check data type
+
+  // var a=100;
+  // print(a.runtimeType);
+
+  // arithmetice operator
+  // var a =10;
+  // var b =20;
+  // var c =30;
+  // var d =5;
+  // print(a+b);
+  // print(c-a);
+  // print(a*d);
+  // print(a/b);
+  // print(a~/d);
+  // print(c%a);
+
+  var n = 1;
+
+  do {
+    print("number $n");
+
+    n++;
+  } while (n >= 3);
+}
